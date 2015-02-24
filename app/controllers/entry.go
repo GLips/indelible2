f7ff628a644ae7008a7f756c95e0fcb1d6e5c3cb
GLips <glipsman@gmail.com>
2015-02-23 23:30:54 -0700

    Add a route to retrieve and return JSON entries individually.

--- app/controllers/entry.go
+++ app/controllers/entry.go
@@ -15,6 +15,8 @@ func (c Entry) Index() revel.Result {
 		// TODO: Abstract this out into a basicQuery function
 		// TODO: Hook up an ESP
 		// TODO: Send emails to welcome users after registering
+		// TODO: Only create entries when content > 0
+		// TODO: CSRF
 		// We initialize entries like this so RenderJSON renders a
 		// blank array as [] instead of null.
 		entries := make([]entry.Entry, 0)
@@ -26,6 +28,21 @@ func (c Entry) Index() revel.Result {
 	} else {
 		return c.RenderJSONError("You need to login first to retrieve a list of your entries.")
 	}
+}
+
+func (c Entry) Get(id int) revel.Result {
+	// TODO: Abstract this out into a basicGet function
+	var e entry.Entry
+	currentUser := c.ActiveUser()
+	connection := db.New()
+	connection.First(&e, id)
+	if c.IsLoggedIn() && currentUser.Id == e.UserId {
+		return c.RenderJSON(map[string]interface{}{e.One(): e})
+	} else {
+		return c.RenderJSONError("You're not authorized to retrieve this entry.")
+	}
+	revel.ERROR.Printf("%+v", id)
+	return nil
 }
 
 func (c Entry) Create() revel.Result {
